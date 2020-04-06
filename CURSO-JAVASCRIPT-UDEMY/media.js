60c363b39ff05fd15fbaa3bb44f92f3b8303bae6
Lucas Lima <lucaslim.lux@gmail.com>
2020-04-06 19:20:36 -0300

    Upando fim curso js

---
+++ CURSO-JAVASCRIPT-UDEMY/media.js
@@ -0,0 +1,7 @@
+function media(...numeros){
+    if(numeros[0] === undefined) return 0;
+    var total = numeros
+        .map( n => parseFloat(n) )
+        .reduce( (a, b) => a+b );
+    return total / numeros.length;
+}
